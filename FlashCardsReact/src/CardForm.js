ba0888402c40d1847f01199b1520e91d3bb4ebbf
User <96498245+UserName2231@users.noreply.github.com>
2026-05-09 04:15:18 +0300

    Add files via upload

---
+++ FlashCardsReact/src/CardForm.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+class CardForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            q: '',
+            a: ''
+        }
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.editData && this.props.editData !== prevProps.editData) {
+            this.setState({
+                q: this.props.editData.q,
+                a: this.props.editData.a
+            })
+        }
+    }
+
+    handleAdd() {
+        let q = this.state.q.trim()
+        let a = this.state.a.trim()
+        if (q === "" || a === "") return
+        this.props.onAdd(q, a)
+        this.setState({ q: '', a: '' })
+    }
+
+    render() {
+        return (
+            <div className="box">
+                <h3>{this.props.editData ? 'Редактировать карточку' : 'Добавить карточку'}</h3>
+                <input
+                    type="text"
+                    placeholder="Вопрос (лицевая)"
+                    value={this.state.q}
+                    onChange={e => this.setState({ q: e.target.value })}
+                />
+                <input
+                    type="text"
+                    placeholder="Ответ (оборотная)"
+                    value={this.state.a}
+                    onChange={e => this.setState({ a: e.target.value })}
+                />
+                <button onClick={this.handleAdd}>
+                    {this.props.editData ? 'Сохранить' : 'Добавить'}
+                </button>
+            </div>
+        )
+    }
+}
+
+export default CardForm
---
+++ FlashCardsReact/src/CardTable.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class CardTable extends React.Component {
+    render() {
+        let rows = this.props.cards.map(card => {
+            return (
+                <tr key={card.id}>
+                    <td>{card.q}</td>
+                    <td>{card.a}</td>
+                    <td>
+                        <input
+                            type="checkbox"
+                            checked={card.done}
+                            onChange={() => this.props.onToggle(card.id)}
+                        />
+                    </td>
+                    <td>
+                        <button onClick={() => this.props.onEdit(card.id)}>Ред</button>
+                        <button onClick={() => this.props.onDelete(card.id)}>Удал</button>
+                    </td>
+                </tr>
+            )
+        })
+
+        return (
+            <div className="box">
+                <h3>Список всех карточек</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Вопрос</th>
+                            <th>Ответ</th>
+                            <th>Выучено</th>
+                            <th>Действия</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default CardTable
